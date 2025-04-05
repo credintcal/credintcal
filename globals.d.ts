7a37b3af75db58f50200b0af78de7caaa9391a2f
Nandagiri <nad.nandagiri@gmail.com>
2025-04-05 13:55:19 +0530

    Add globals.d.ts for TypeScript declarations

---
+++ globals.d.ts
@@ -0,0 +1,8 @@
+// This file contains global type declarations
+
+// Extend the Window interface to include Razorpay
+interface Window {
+  Razorpay: any;
+}
+
+// If you need to declare any other global types, add them here 
