a0b2af5ffbafd552618af8d06b53d33ffb57904d
Nandagiri <nad.nandagiri@gmail.com>
2025-04-06 21:25:44 +0530

    Simplify build configuration for better Vercel compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: process.env.NEXT_PUBLIC_DISABLE_OPTIMIZE_IMGS === '1',
+    unoptimized: true,
   },
   experimental: {
     optimizeCss: true,
